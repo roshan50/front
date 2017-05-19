6c635ab0f9ab9bd9c438d1ffa83894870cbc3744
roshan <m.roshan50@yahoo.com>
2017-05-19 10:48:09 +0430

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// var mybtn = document.querySelector('.btn');
+//
+//
+//
+// var sayHi = function  () {
+//     console.log('hi');
+// }
+
+array = [1,2,3,4];
+
+var mappedArray = array.map(function (item) {
+    return 2
+})
+
+console.log(mappedArray)
+
+
+// console.log(sayHi);
+//
+// mybtn.addEventListener('click', sayHi)
